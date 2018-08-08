ab9f227ec97adb4a72db598db891885207667b59
Lawan Petza <Lawan.Petza@gmail.com>
2018-08-07 21:10:48 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import Calendar from "./components/calendar";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      <Calendar>
+
+      </Calendar>
+
+
+      
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/calendar/calendar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./calendar.css";
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import "react-big-calendar/lib/css/react-big-calendar.css"
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+
+BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
+
+
+const MyCalendar = props => (
+  <div style = {{height: 600}}>
+    <BigCalendar
+      events={[
+        {
+            'title': "Tom Walsh",
+            'start': new Date(2018, 6, 27), 
+            'end': new Date(2018, 6, 27), 
+            'allDay': false 
+        },
+        {
+            'title': "Paul Lover, Will Pittman",
+            'start': new Date(2018, 6, 28), 
+            'end': new Date(2018, 6, 28), 
+            'allDay': false 
+        },
+        {
+            'title': "Kevin Bidges, Chris Feeree",
+            'start': new Date(2018, 7, 3), 
+            'end': new Date(2018, 7, 3), 
+            'allDay': false 
+        },
+        {
+            'title': "Doug Williams, DT",
+            'start': new Date(2018, 7, 4), 
+            'end': new Date(2018, 7, 4), 
+            'allDay': false 
+        },
+        {
+            'title': "Jay Corriher, David Whitman",
+            'start': new Date(2018, 7, 10), 
+            'end': new Date(2018, 7, 10), 
+            'allDay': false 
+        },
+        {
+            'title': "Katie Oakes, Parker Foley",
+            'start': new Date(2018, 7, 11), 
+            'end': new Date(2018, 7, 11), 
+            'allDay': false 
+        },
+
+      ]}
+
+      step={60}
+      view='month'
+      views={['month']}
+      onView={() => {}}
+  
+    />
+  </div>
+);
+
+
+
+
+export default MyCalendar;
